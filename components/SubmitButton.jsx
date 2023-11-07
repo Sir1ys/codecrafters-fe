2efd961f5e3451d89b23f1c8e8e327ce30391c5c
Sir1ys <sashakoroviy@gmail.com>
2023-11-07 10:13:55 +0000

    sign in form was refactored

---
+++ components/SubmitButton.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+import { colors } from "../constants/colors";
+
+export const SubmitButton = (props) => {
+  const enabledBgColor = props.color || colors.primary;
+  const disabledBgColor = colors.lightGrey;
+  const bgColor = props.disabled ? disabledBgColor : enabledBgColor;
+
+  return (
+    <TouchableOpacity
+      onPress={props.disabled ? () => {} : props.onPress}
+      style={{ ...styles.button, ...{ backgroundColor: bgColor } }}
+    >
+      <Text style={{ color: props.disabled ? colors.grey : "white" }}>
+        {props.text}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+  },
+});
