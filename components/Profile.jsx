0dbabfa8865815e0b02fbd8ea528e965e8311872
Sir1ys <sashakoroviy@gmail.com>
2023-11-08 14:27:00 +0000

    Log out button was added

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text } from "react-native";
+import { SubmitButton } from "./SubmitButton";
+import { UserContext } from "../contexts/UserContext";
 
 export const Profile = () => {
+  const { userState, userAuth } = useContext(UserContext);
+  const setUser = userState[1];
+  const setUserAuthenticated = userAuth[1];
+
+  const handleLogOut = () => {
+    setUser({});
+    setUserAuthenticated(false);
+  };
+
   return (
     <View>
       <Text>This is a Profile!!</Text>
+      <SubmitButton
+        disabled={false}
+        text="Log out"
+        onPress={() => handleLogOut()}
+      />
     </View>
   );
 };
