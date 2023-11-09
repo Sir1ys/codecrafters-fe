8a585c8a1e324e4c284262ae098792452fd4ecd9
Jookz <j.chatterton456@gmail.com>
2023-11-09 11:33:51 +0000

    created feed page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,17 +1,40 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Button, ScrollView } from "react-native";
+import { fetchEvents } from "../api";
+import moment from "moment";
 
 export const Feed = (props) => {
+  const [eventList, setEventList] = useState([]);
+
+  useEffect(() => {
+    fetchEvents().then(({ events }) => {
+      setEventList(events);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>This is a home page</Text>
-      <Button
-        title="Go to card"
-        onPress={() => {
-          props.navigation.navigate("Card");
-        }}
-      ></Button>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text>Feed</Text>
+        <View style={styles.container}>
+          {eventList.map((event) => {
+            return (
+              <View style={[styles.event, styles.shadowProp]}>
+                <Text style={{ textAlign: "center", fontWeight: "700" }}>
+                  {event.short_description}
+                </Text>
+                <Text>{event.location}</Text>
+                <Text>{event.country}</Text>
+                <Text>{moment(event.date).format("DD/MM/YYYY")}</Text>
+                <Text>{event.description}</Text>
+                <Button title="Save"></Button>
+                <Button title="See event"></Button>
+              </View>
+            );
+          })}
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -20,5 +43,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    paddingHorizontal: 10,
+  },
+  event: {
+    borderWidth: 3,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    marginVertical: 10,
+    padding: 20,
+  },
+  shadowProp: {
+    shadowColor: "#000000",
+    shadowOffset: { width: -10, height: 100 },
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
   },
 });
---
+++ components/SingleEvent.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SingleEvent() {
+  return <div>SingleEvent</div>;
+}
